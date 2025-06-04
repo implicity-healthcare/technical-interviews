a12bb84a70dc363176ee804e5c78ad335e356db9
Julien Sarazin <julien.sarazin@implicity.com>
2025-06-04 14:27:10 +0200

    chore(1.0.1): Updated test configuration

--- app/src/app/pages/pages.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { PatientListPage } from './patients/patients';
-
-export default [
-    { path: 'patients', component: PatientListPage },
-] as Routes;
